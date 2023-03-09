1cea3b73ef867a94de47721b1b62f342ee37a963
Abner Freitas <defreitasabner@gmail.com>
2023-03-09 14:56:14 -0300

    Implemented object ScrapedVideoInfo .

--- src/tube_scrapy/tube_scrapy.py
+++ src/tube_scrapy/tube_scrapy.py
@@ -1,10 +1,12 @@
-from typing import List, Dict
+from typing import List
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
+from scraped_video_info import ScrapedVideoInfo
 
 class TubeScrapy:
     def __init__(self) -> None:
@@ -21,7 +23,7 @@ class TubeScrapy:
     def search_video_info(self, 
                       query: str, 
                       add_to_search: str = None, 
-                      search_range: int = 0) -> List[Dict[str, str]]:
+                      search_range: int = 0) -> List[ScrapedVideoInfo]:
         """
         Method which webscraping Youtube to return video info. The search is based in a `query` with the name of video. You could `add_to_search` to add a term to the main `query`. The argument `search_range` determine how many videos info would be returned based in order which the videos appears on Youtube page.
         """
@@ -45,11 +47,11 @@ class TubeScrapy:
             video_url = video.find_element(By.TAG_NAME, 'a').get_property('href')
             thumbnail = video.find_element(By.TAG_NAME, 'img').get_property('src')
             views = video.find_element(By.ID, 'metadata-line').text.split('\n')[0]
-            video_info = {
-                'title': title,
-                'url': video_url,
-                'thumbnail': thumbnail,
-                'views': views,
-            }
+            video_info = ScrapedVideoInfo(
+                title = title,
+                video_url = video_url,
+                thumbnail_url= thumbnail,
+                views = views
+            )
             videos_infos.append(video_info)
         return videos_infos
