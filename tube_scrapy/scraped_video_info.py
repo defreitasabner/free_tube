52202243858a58235fa806e6471f86ad659bff32
Abner Freitas <defreitasabner@gmail.com>
2023-03-11 20:56:27 -0300

    Fixed bug with class definition

--- tube_scrapy/scraped_video_info.py
+++ tube_scrapy/scraped_video_info.py
@@ -3,9 +3,9 @@ class ScrapedVideoInfo:
     An object which contains scraped data from a Youtube's Video.
     """
     def __init__(self, title: str, video_url: str, thumbnail_url: str, views: str) -> None:
-        self.__title: str = title,
-        self.__video_url: str = video_url,
-        self.__thumbnail_url: str = thumbnail_url,
+        self.__title: str = title
+        self.__video_url: str = video_url
+        self.__thumbnail_url: str = thumbnail_url
         self.__views: str =  views
 
     def __repr__(self) -> str:
